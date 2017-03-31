5a2c6d1cf040ad8b28cd1501c90736e5ace7b808
Sean Brage <sean@layer3tv.com>
2017-03-31 01:28:30 -0600

    Refactored to dynamically populate templates.

--- SMB.js
+++ SMB.js
@@ -6,7 +6,16 @@ hbs.registerPartials(`${__dirname}/views/partials`);
 app.set('view engine', 'hbs');
 
 app.get('/', (req, res) => {
-  res.render('resume');
+  res.render('resume', {
+    history: {
+      jobs: require('./api/resume/history_work.json'),
+      education: require('./api/resume/history_education.json')
+    },
+    skills: {
+      tech: require('./api/resume/skills_tech.json'),
+      soft: require('./api/resume/skills_soft.json')
+    }
+  });
 });
 
 app.listen(8080, () => {

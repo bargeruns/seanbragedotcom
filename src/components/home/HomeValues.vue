ff5a9006385c06f141a72886296a090aa44ab527
Sean Brage <seanmbrage@me.com>
2020-03-10 21:17:40 -0600

    refactor HomeValues using v-for

--- src/components/home/HomeValues.vue
+++ src/components/home/HomeValues.vue
@@ -1,50 +1,15 @@
 <template>
   <ContentBlock class="home-values">
     <TriggerBox
-      :selected="selectedValue === 'thoughtfulness'"
-      @selected="setSelectedValue"
-      name="thoughtfulness"
+      v-for="value in values"
+      :key="value.title"
+      :selected="selectedValue === value.name"
     >
       <template v-slot:icon>
-        <font-awesome-icon :icon="['fas', 'hat-wizard']"></font-awesome-icon>
+        <font-awesome-icon :icon="value.icon"></font-awesome-icon>
       </template>
-      <template v-slot:title>Thoughtfulness</template>
-      <template v-slot:detail>
-        Use the right tool for the job. Code to the concern, not the developer's
-        personal preference. Do what's right for customer and company. I do my
-        best to bring a high level of intentionality and deliberation to the
-        table.
-      </template>
-    </TriggerBox>
-    <TriggerBox
-      :selected="selectedValue === 'strategy'"
-      @selected="setSelectedValue"
-      name="strategy"
-    >
-      <template v-slot:icon>
-        <font-awesome-icon :icon="['fas', 'wrench']"></font-awesome-icon>
-      </template>
-      <template v-slot:title>Strategy</template>
-      <template v-slot:detail>
-        Weeks of coding can save you hours of planning. Before each task,
-        feature, and project I strive to clarify outcomes, account for edge
-        cases, and formulate a game plan.
-      </template>
-    </TriggerBox>
-    <TriggerBox
-      :selected="selectedValue === 'creativity'"
-      @selected="setSelectedValue"
-      name="creativity"
-    >
-      <template v-slot:icon>
-        <font-awesome-icon :icon="['fas', 'paint-brush']"></font-awesome-icon>
-      </template>
-      <template v-slot:title>Creativity</template>
-      <template v-slot:detail>
-        Let's get weird with it. Sometimes the difference between the right
-        solution and "the way we've always done it" is a million miles. Don't
-        reinvent the wheel, unless the wheel sucks. In that case, let's make
-        ourselves a badass new wheel.
+      <template v-slot:title>
+        {{ value.name }}
       </template>
     </TriggerBox>
   </ContentBlock>
@@ -58,7 +23,21 @@ export default {
   components: { TriggerBox, ContentBlock },
   data() {
     return {
-      selectedValue: "thoughtfulness"
+      selectedValue: "thoughtfulness",
+      values: [
+        {
+          icon: ["fas", "hat-wizard"],
+          name: "thoughtfulness"
+        },
+        {
+          name: "strategy",
+          icon: ["fas", "wrench"]
+        },
+        {
+          name: "creativity",
+          icon: ["fas", "paint-brush"]
+        }
+      ]
     };
   },
   methods: {
